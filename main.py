47af298580bbb02219de4728f878bc1b5c93b36d
samuelbaiju <baijusamuel10@gmail.com>
2026-01-29 13:23:56 +0530

    cors issue

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ app = FastAPI(title="Translation Webhook API", description="A webhook for transl
 # Allow CORS for frontend (adjust origins as needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://ai-text-translator-two.vercel.app/",
+        "https://ai-text-translator.onrender.com",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"] ,
     allow_headers=["*"]
